21bfa46c1269f5aea728f999c481ad80a9e137e2
Luis Henrique <euluishenrique7@gmail.com>
2024-04-07 15:30:33 -0300

    Mudança de Design

--- app/src/main/java/com/luis/pi_app/MainActivity.java
+++ app/src/main/java/com/luis/pi_app/MainActivity.java
@@ -1,34 +1,31 @@
 package com.luis.pi_app;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import androidx.activity.EdgeToEdge;
+import android.widget.ImageView;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        Button botao_tutorial = findViewById(R.id.botao_tutorial);
-        botao_tutorial.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                ModalTutorial modal = new ModalTutorial(MainActivity.this);
-                modal.show();
-            }
-        });
+        ImageView botao_tutorial = findViewById(R.id.botao_tutorial);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        botao_tutorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                LayoutInflater inflater = getLayoutInflater();
+                View dialogView = inflater.inflate(R.layout.modal_tutorial, null);
+                builder.setView(dialogView);
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
         });
     }
 }
